2e4d7f3d197e95ec33e5d9fbd426918ad877860c
Martín René <mrvilugron@unrn.edu.ar>
2022-05-19 11:02:38 -0300

    agregada configuracion para ejecutar tests con cobertura

---
+++ test.py
@@ -0,0 +1,12 @@
+# content of myinvoke.py
+import pytest
+import sys
+
+
+class MyPlugin:
+    def pytest_sessionfinish(self):
+        print("*** test run reporting finishing")
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main())
